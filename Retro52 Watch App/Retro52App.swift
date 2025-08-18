56e1defc47b246635caed5db1791e0adea85c3ba
Gunish Sharma <gunishsharma20@gmail.com>
2025-08-18 17:18:23 -0600

    Initial Commit

---
+++ Retro52 Watch App/Retro52App.swift
@@ -0,0 +1,17 @@
+//
+//  Retro52App.swift
+//  Retro52 Watch App
+//
+//  Created by Gunish Sharma on 2025-08-18.
+//
+
+import SwiftUI
+
+@main
+struct Retro52_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
